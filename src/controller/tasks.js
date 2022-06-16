ccd6c8590201a536ebe99d1c9247a496682bb330
AlanJSimpson <allanstark2908@gmail.com>
2022-06-16 20:34:13 -0300

    Criado rota para completar/update task

--- src/controller/tasks.js
+++ src/controller/tasks.js
@@ -1,7 +1,7 @@
 const TaskModel = require('../model/taskModel/taskModel');
 
 exports.getAllTasksData = async (_req, res) => {
-  const allTasks = await TaskModel.find({});
+  const allTasks = await TaskModel.find({ status: false });
   res.send({ dados: allTasks });
 };
 
@@ -12,13 +12,21 @@ exports.createTaskData = async (req, res) => {
     description,
   });
 
-  res.send({ saved: true });
+  res.status(201).send({ saved: true });
 };
 
 exports.deleteTaskData = async (req, res) => {
   const { id } = req.params;
 
   await TaskModel.deleteOne({ _id: id });
+
+  res.status(204).end();
+};
+
+exports.updateTaskData = async (req, res) => {
+  const { id } = req.params;
+
+  await TaskModel.updateOne({ _id: id }, { status: true });
 
   res.end();
 };
--- src/router/taskRouters.js
+++ src/router/taskRouters.js
@@ -3,11 +3,13 @@ const {
   getAllTasksData,
   createTaskData,
   deleteTaskData,
+  updateTaskData,
 } = require('../controller/tasks');
 const taskRouters = express.Router();
 
 taskRouters.get('/', getAllTasksData);
 taskRouters.post('/', createTaskData);
 taskRouters.delete('/:id', deleteTaskData);
+taskRouters.put('/:id', updateTaskData);
 
 module.exports = taskRouters;
