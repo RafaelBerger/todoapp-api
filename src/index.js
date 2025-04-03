ae08e8e8297c7fdfac843f027da7b5f27409a196
Rafael <rafaelberger.dev@gmail.com>
2025-04-03 03:53:04 -0300

    porta mudada

--- src/index.js
+++ src/index.js
@@ -27,6 +27,8 @@ app.use(express.json());
 
 app.use('/', taskRouters);
 
-app.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Servidor de pé');
 });
